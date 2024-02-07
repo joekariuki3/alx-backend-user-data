6e6f96a22957712948dc19ceaa727f365213a268
joekariuki3 <joekariuki3@gmail.com>
2024-02-07 21:01:25 +0300

    Regex-ing

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -8,6 +8,5 @@ import re
 def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
     """return a message with important info obfuscated"""
-    # pattern = '|'.join(fields)
     regex = re.compile(f'("|".join(fields))=([^{separator}]*)')
     return regex.sub(fr'\1={redaction}', message)
