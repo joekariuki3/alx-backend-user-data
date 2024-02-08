79861ed3f809c182f0d5ea40a54d73b37600a0aa
joekariuki3 <joekariuki3@gmail.com>
2024-02-08 11:40:29 +0300

    use Regex ti find sensitive info and obfuscate it

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,19 +1,14 @@
 #!/usr/bin/env python3
-"""function that returns the log message obfuscated
-    Arguments:
-        fields: a list of strings representing all fields to obfuscate
-        redaction -> r: a string representing by what the field
-                        will be obfuscated
-        message -> msg: a string representing the log line
-        separator -> sep: a string representing by which character is
-                          separating all fields in the log line (message)
-"""
+"""Use of regex in replacing occurrences of certain field values"""
 
 from typing import List
 import re
 
 
-def filter_datum(fields: List[str], r: str, msg: str, sep: str) -> str:
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
     """return a message with important info obfuscated"""
-    return re.sub(fr'({"|".join(fields)})=[^{sep}]*',
-                  lambda x: x.group(1) + "=" + r, msg)
+    for field in fields:
+        message = re.sub(f'{field}=(.*?){separator}',
+                         f'{field}={redaction}{separator}', message)
+        return message
