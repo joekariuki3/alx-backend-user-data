7a306ed2969f716b9d6bbb3f728bdc741ed8ad52
joekariuki3 <joekariuki3@gmail.com>
2024-02-12 23:02:43 +0300

    Basic - User object

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,7 +1,9 @@
-#!/usr/bin/env python3
-"""BasicAuth class inherits from Auth"""
+#!/usr/bin/env
+"""BasicAuth class"""
 
+from typing import TypeVar
 from api.v1.auth.auth import Auth
+from models.user import User
 import base64
 
 
@@ -48,3 +50,16 @@ class BasicAuth(Auth):
         email = dbah_list[0]
         password = dbah_list[1]
         return (email, password)
+
+    def user_object_from_credentials(self, user_email: str, user_pwd: str) -> TypeVar('User'):
+        """Returns a user or None"""
+        if not user_email or not isinstance(user_email, str):
+            return None
+        if not user_pwd or not isinstance(user_pwd, str):
+            return None
+        result, key_found = User.search(User, email=user_email)
+        if not result or not key_found:
+            return None
+        if user.is_valid_password(user_pwd):
+            return user
+        return None
