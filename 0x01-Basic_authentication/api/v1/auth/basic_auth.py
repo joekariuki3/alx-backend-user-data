a876bff79d7d99708fc9475de540487b6b669957
joekariuki3 <joekariuki3@gmail.com>
2024-02-12 15:03:34 +0300

    implement a decode method
    returns a clean utf 8  string from base64 byte string or None

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,7 @@
 """BasicAuth class inherits from Auth"""
 
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -19,3 +20,16 @@ class BasicAuth(Auth):
         if not ah_list or ah_list[0] != "Basic" or len(ah_list) < 2:
             return None
         return ah_list[1]
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header: str) -> str:
+        """Returns a base64 decoded string or None"""
+        bah = base64_authorization_header
+        if not bah or not isinstance(bah, str):
+            return None
+        try:
+            auth_string = base64.b64decode(bah)
+            clean_string = auth_string.decode('utf-8')
+        except base64.binascii.Error:
+            return None
+        return clean_string
