4bb2c64d8392df3e5020c6a37712a8840ce363f3
joekariuki3 <joekariuki3@gmail.com>
2024-02-12 14:35:58 +0300

    extract authentication value
    from the header key and check if it is
    Basic authentication

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -8,3 +8,14 @@ class BasicAuth(Auth):
 
     def __init__(self):
         """initilize BasicAuth"""
+
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """returns base64 auth value or None"""
+        ah = authorization_header
+        if not ah or not isinstance(ah, str):
+            return None
+        ah_list = ah.split(" ")
+        if not ah_list or ah_list[0] != "Basic" or len(ah_list) < 2:
+            return None
+        return ah_list[1]
