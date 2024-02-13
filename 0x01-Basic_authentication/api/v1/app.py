4a58111cf88dfa21ed8c640554d0c9383b631a3a
joekariuki3 <joekariuki3@gmail.com>
2024-02-13 12:59:32 +0300

    basic auth environment variable

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -15,7 +15,7 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
-auth_type = os.getenv("AUTH_TYPE")
+auth_type = os.getenv("AUTH_TYPE", "auth")
 
 if auth_type:
     if auth_type == "basic_auth":
