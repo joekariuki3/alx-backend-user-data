a1731a69ae2138ccd95ceaa86491443eb692fff1
joekariuki3 <joekariuki3@gmail.com>
2024-02-15 13:38:28 +0300

    New view for Session Authentication correction

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+
 """Module for session views
 """
 from api.v1.views import app_views
@@ -6,41 +7,40 @@ from flask import request, jsonify, make_response
 from models.user import User
 from os import getenv
 
+
 @app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
 def log_in():
     """ceates a session if a user is authenticated
     """
-    print("hello")
-    print(request.form)
-    if request.form:
-        email = request.form.get('email')
-        password = request.form.get('password')
-        if not email:
-            error_data = { "error": "email missing" }
-            print('mail missing')
-            return jsonify(error_data), 400
-        if not password:
-            error_data = { "error": "password missing" }
-            return jsonify(error_data), 400
-        # retrieve user based on the email
-        try:
-            user = User.search(email=email)[0]
-        except KeyError:
-            error_data = { "error": "no user found for this email" }
-            return jsonify(error_data), 404
-        if not user:
-            error_data = { "error": "no user found for this email" }
-            return jsonify(error_data), 404
-        # check if password is valid
-        if not user.is_valid_password(password):
-            error_data = { "error": "wrong password" }
-            return jsonify(error_data), 401
-        # creat a sessionID for the user ID
-        from api.v1.app import auth
-        session_id = auth.create_session(user.id)
-        # set the cookie to the response
-        session_name = getenv("SESSION_NAME")
-        response = make_response(user.to_json())
-        response.set_cookie(session_name, session_id)
-        return response
-
+    email = request.form.get('email', None)
+    password = request.form.get('password', None)
+    if not email:
+        error_data = {"error": "email missing"}
+        return jsonify(error_data), 400
+    if not password:
+        error_data = {"error": "password missing"}
+        return jsonify(error_data), 400
+    # retrieve user based on the email
+    user = None
+    try:
+        result = User.search({"email": email})
+        if len(result) > 0:
+            user = result[0]
+    except KeyError:
+        error_data = {"error": "no user found for this email"}
+        return jsonify(error_data), 404
+    if not user:
+        error_data = {"error": "no user found for this email"}
+        return jsonify(error_data), 404
+    # check if password is valid
+    if not user.is_valid_password(password):
+        error_data = {"error": "wrong password"}
+        return jsonify(error_data), 401
+    # creat a sessionID for the user ID
+    from api.v1.app import auth
+    session_id = auth.create_session(user.id)
+    # set the cookie to the response
+    session_name = getenv("SESSION_NAME")
+    response = make_response(user.to_json())
+    response.set_cookie(session_name, session_id)
+    return response
