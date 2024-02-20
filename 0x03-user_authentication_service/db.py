ce4a1ab4f2096901d67621092109e9926f7d88c2
joekariuki3 <joekariuki3@gmail.com>
2024-02-20 12:15:25 +0300

    create user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -38,7 +38,7 @@ class DB:
             new_user = User(email=email, hashed_password=hashed_password)
             self._session.add(new_user)
             self._session.commit()
-        except Exception:
+        except Exception as e:
             self._session.rollback()
             raise
         return new_user
