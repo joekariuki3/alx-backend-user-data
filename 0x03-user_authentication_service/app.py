4668140e7281f265b67afa2c7346fcc33b0fea78
joekariuki3 <joekariuki3@gmail.com>
2024-02-22 11:03:06 +0300

    Update password end-point correction

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -86,7 +86,7 @@ def get_reset_password_token() -> str:
         abort(403)
 
 
-@app.route('reset_password', methods=['PUT'], strict_slashes=False)
+@app.route('/reset_password', methods=['PUT'], strict_slashes=False)
 def update_password() -> str:
     """updates a users password that emails matches the one passed
     """
